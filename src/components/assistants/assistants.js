c88b61169259654389baaba8c0e779a9c4d6b204
victor sada <victorjsada@gmail.com>
2021-08-03 23:38:12 -0300

    WIP: agregando el asistente

--- src/components/assistants/assistants.js
+++ src/components/assistants/assistants.js
@@ -1,9 +1,43 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import Assistant from "./assistant";
 
 const Assistants = ({ auth }) => {
   const token = localStorage.getItem("token");
+  const [control, setControl] = useState(false);
   const [assistant, setAssistant] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [member, setMember] = useState(false);
+  const [error, setError] = useState(false);
+  const [asistente, setAsistente] = useState({
+    name: "",
+    email: "",
+    birthday: "",
+    adress: "",
+    telf: "",
+    sex: "",
+  });
+  const { name, email, birthday, adress, telf } = asistente;
+  asistente.member = member;
+  console.log("CONTROL: ", control);
+  useEffect(() => {
+    console.log("EMPEZANDO EL useEffect", control);
+    if (control === false) {
+      return;
+    }
+    const createAssistant = async () => {
+      await fetch("http://localhost:400/api/assistant", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+        body: JSON.stringify(asistente),
+      });
+    };
+    createAssistant();
+    console.log("TERMINANDO EL useEffect");
+  }, [control]);
 
   useEffect(() => {
     const getAssistants = async () => {
@@ -17,8 +51,40 @@ const Assistants = ({ auth }) => {
       const participant = await response.json();
       setAssistant(participant);
     };
+    setControl(false);
+    console.log("TERMINO EL USE EFFECT CON EL CONTROL EN ", control);
     getAssistants();
   }, [token]);
+
+  const handleSubmit = async () => {
+    if (name.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    setAsistente({
+      name: "",
+      email: "",
+      birthday: "",
+      adress: "",
+      telf: "",
+      sex: "",
+    });
+    setControl(true);
+    console.log("paso por el control");
+    setError(false);
+    setModal(false);
+  };
+
+  const handleChange = (e) => {
+    setAsistente({
+      ...asistente,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const memberChange = (e) => {
+    setMember(!member);
+  };
   return (
     <Fragment>
       {token || auth ? (
@@ -47,10 +113,116 @@ const Assistants = ({ auth }) => {
               )}
             </tbody>
           </table>
+          <div className="derecha">
+            <button
+              type="button"
+              onClick={() => setModal(true)}
+              className="btn btn-success"
+            >
+              +
+            </button>
+          </div>
         </div>
       ) : (
         <h1>No estas autorizado, ¡ve y logueate! :D</h1>
       )}
+      <Modal isOpen={modal}>
+        <ModalHeader>
+          <h4>Crea un Asistente</h4>
+        </ModalHeader>
+        <ModalBody>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Nombre del Asistente..."
+              value={name}
+              onChange={handleChange}
+            />
+            {error ? (
+              <p className="p-2 mt-2 bg-danger text-light">
+                <b> El nombre es obligatorio</b>
+              </p>
+            ) : null}
+            <input
+              type="email"
+              className="form-control mt-2"
+              name="email"
+              placeholder="Email del Asistente..."
+              value={email}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              className="form-control mt-2"
+              name="adress"
+              placeholder="Direccion Asistente..."
+              value={adress}
+              onChange={handleChange}
+            />
+            <input
+              type="date"
+              className="form-control mt-2"
+              name="birthday"
+              placeholder="Fecha de nacimiento Asistente..."
+              value={birthday}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              className="form-control mt-2"
+              name="telf"
+              placeholder="Telefono Asistente..."
+              value={telf}
+              onChange={handleChange}
+            />
+            <input
+              type="radio"
+              className="mt-2"
+              name="sex"
+              onChange={handleChange}
+              value="masculino"
+            />{" "}
+            Masculino{" "}
+            <input
+              type="radio"
+              className="mt-2"
+              name="sex"
+              onChange={handleChange}
+              value="femenino"
+            />{" "}
+            Femenino <br />
+            <input
+              type="checkbox"
+              className=" mt-2"
+              name="member"
+              checked={member}
+              onChange={memberChange}
+            />{" "}
+            Miembro de la Iglesia
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            className="btn btn-success btn-block"
+            type="button"
+            onClick={handleSubmit}
+          >
+            Crear
+          </button>
+          <button
+            onClick={() => {
+              setModal(false);
+              setError(false);
+            }}
+            className="btn btn-block btn-danger"
+            type="button"
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
     </Fragment>
   );
 };
